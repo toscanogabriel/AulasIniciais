88651cbce85215bd836f52d017ac92d586d1b697
Gabriel <gabriel_toscano@outlook.com>
2025-03-19 21:06:38 -0300

    docs: atualização do README.md. docs: atualização do arquivo Aula02. docs: adição do arquivo Aula03.

--- Aula03ExercicioTiposPrimitivos.java
+++ Aula03ExercicioTiposPrimitivos.java
@@ -5,7 +5,7 @@ public class Aula03ExercicioTiposPrimitivos {
         String nome = "Gabriel";
         String endereco = "Rua Gabriel, nº 123";
         double salario = 2500.0d;
-                String recebimentoSalario = "19/03/2025";
+        String recebimentoSalario = "19/03/2025";
         String relatorio = "Eu, " + nome + ", morando no endereço" + endereco + " confirmo que recebi o salário de R$" + salario + ", na data " + recebimentoSalario;
 
         System.out.println(relatorio);
